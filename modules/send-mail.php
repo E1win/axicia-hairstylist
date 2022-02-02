b879f71a204b3b720b2af90c4ebeddea58a3399f
E1win <87700553+E1win@users.noreply.github.com>
2022-02-02 16:04:04 +0100

    almost finished contact page, refactored card item
    Contact page is pretty much finished in the HTML and CSS side of things, need to create email validation and finish the php file.
    
    Refactored some of the things in common from both of the card's items in one class

---
+++ modules/send-mail.php
@@ -0,0 +1,14 @@
+<?php
+
+// Validate information
+
+// Send email
+
+// If one of those things don't work, send to new page with failure message (make it look decent)
+
+// If it works, send back to contact.html with success message
+
+
+header("Location: ../public/contact.html");
+
+?>
