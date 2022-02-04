01e5dbd0a9f87bb0d63ade12f1d692ed1ae6c8e0
E1win <87700553+E1win@users.noreply.github.com>
2022-02-04 14:12:14 +0100

    created contact-send.php
    Contact send is the page where people will land after sending the contact form.
    
    The page changes depending on the outcome of the mail, it shows errors or success messages

--- modules/send-mail.php
+++ modules/send-mail.php
@@ -1,14 +1,6 @@
 <?php
 
-// Validate information
 
-// Send email
-
-// If one of those things don't work, send to new page with failure message (make it look decent)
-
-// If it works, send back to contact.html with success message
-
-
-header("Location: ../public/contact.html");
+//header("Location: ../public/contact.html");
 
 ?>
